f9b54b25f91f8b89d42be5a5276af8f47b1da193
onuruci <onur.yuce@metu.edu.tr>
2023-04-29 19:13:15 +0300

    fixed imports

--- packages/relay/src/routes/upvote.ts
+++ packages/relay/src/routes/upvote.ts
@@ -5,33 +5,33 @@ import { Synchronizer } from '@unirep/core'
 import { EpochKeyProof } from '@unirep/circuits'
 import { APP_ADDRESS } from '../config'
 import TransactionManager from '../singletons/TransactionManager'
-import UNIREP_APP from '@unirep-app/contracts/artifacts/contracts/UnirepApp.sol/UnirepApp.json'
+import UNIREP_APP from '@unirep-app/contracts/artifacts/contracts/UnirepApp.sol/ZKComm.json'
 
 export default (app: Express, db: DB, synchronizer: Synchronizer) => {
-    app.post('/api/upvote', async (req, res) => {
-        try {
-            console.log('Upvote entered')
-            const { index } = req.body
+  app.post('/api/upvote', async (req, res) => {
+    try {
+      console.log('Upvote entered')
+      const { index } = req.body
 
-            const appContract = new ethers.Contract(APP_ADDRESS, UNIREP_APP.abi)
+      const appContract = new ethers.Contract(APP_ADDRESS, UNIREP_APP.abi)
 
-            console.log('App contract')
+      console.log('App contract')
 
-            let calldata: any
+      let calldata: any
 
-            calldata = appContract.interface.encodeFunctionData('upVote', [
-                index,
-            ])
+      calldata = appContract.interface.encodeFunctionData('upVote', [
+        index,
+      ])
 
-            const hash = await TransactionManager.queueTransaction(
-                APP_ADDRESS,
-                calldata
-            )
+      const hash = await TransactionManager.queueTransaction(
+        APP_ADDRESS,
+        calldata
+      )
 
-            console.log(hash)
-            res.json({ hash })
-        } catch (error: any) {
-            res.status(500).json({ error })
-        }
-    })
+      console.log(hash)
+      res.json({ hash })
+    } catch (error: any) {
+      res.status(500).json({ error })
+    }
+  })
 }
